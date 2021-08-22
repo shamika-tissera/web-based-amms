ffb6f64ef4aece40561d6ee82a8aceda292a525c
Shamika Tissera <nimeshshamika@gmail.com>
2021-08-22 14:04:16 +0530

    Working on login screen

--- headerNav.php
+++ headerNav.php
@@ -1,3 +1,18 @@
+<?php
+   if (!isset($_SESSION["username"])) {
+      header("location:login.php");
+      exit();
+   }
+   if(isset($_SESSION['firstname'])){
+      $fname = $_SESSION['firstname'];
+   }
+   if(isset($_SESSION['lastname'])){
+      $lname = $_SESSION['lastname'];
+   }
+   if(isset($_SESSION['username'])){
+      $uname = $_SESSION['username'];
+   }
+?>
 <nav class="navbar navbar-light navbar-expand bg-white shadow mb-4 topbar static-top">
                   <div class="container-fluid">
                      <button class="btn btn-link d-md-none rounded-circle me-3" id="sidebarToggleTop" type="button"><i class="fas fa-bars"></i></button>
@@ -105,7 +120,7 @@
                         <div class="d-none d-sm-block topbar-divider"></div>
                         <li class="nav-item dropdown no-arrow">
                            <div class="nav-item dropdown no-arrow">
-                              <a class="dropdown-toggle nav-link" aria-expanded="false" data-bs-toggle="dropdown" href="#"><span class="d-none d-lg-inline me-2 text-gray-600 small">Valerie Luna</span><img class="border rounded-circle img-profile" src="assets/img/avatars/avatar1.jpeg"></a>
+                              <a class="dropdown-toggle nav-link" aria-expanded="false" data-bs-toggle="dropdown" href="#"><span class="d-none d-lg-inline me-2 text-gray-600 small"><?php echo "$fname" . " " . "$lname" ?></span><img class="border rounded-circle img-profile" src="assets/img/avatars/avatar1.jpeg"></a>
                               <div class="dropdown-menu shadow dropdown-menu-end animated--grow-in">
                                  <a class="dropdown-item" href="#"><i class="fas fa-user fa-sm fa-fw me-2 text-gray-400"></i>&nbsp;Profile</a><a class="dropdown-item" href="#"><i class="fas fa-cogs fa-sm fa-fw me-2 text-gray-400"></i>&nbsp;Settings</a><a class="dropdown-item" href="#"><i class="fas fa-list fa-sm fa-fw me-2 text-gray-400"></i>&nbsp;Activity log</a>
                                  <div class="dropdown-divider"></div>
--- includes/login-inc.php
+++ includes/login-inc.php
@@ -1,14 +1,14 @@
 <?php
-
-//determines if the user has accessed this page in the proper way (via the submission form)
-if(!isset($_POST["submit"])){
-    header("location: ../login.php");
+include 'dbh-inc.php';
+$uname = $_POST["uname"];
+$pwd = $_POST["pwd"];
+echo "<script>alert(\"$uname\")</script>";
+if(emptyInputLogin($uname, $pwd) !== false){
+    header("location: ../login.php?error=emptyinput");
     exit();
 }
 
-$uname = $_POST["uname"];
-$pwd = $_POST["pwd"];
-
+loginUser($conn, $uname, $pwd);
 require_once 'dbh-inc.php';
 function emptyInputLogin($uname, $pwd){
     $result = false;
@@ -39,13 +39,15 @@ function checkUnameExists($uname, $conn){
     mysqli_stmt_close($stmt);
 }
 function loginUser($conn, $uname, $pwd){
+    
     $userExists = checkUnameExists($uname, $conn);
     if($userExists === false){
+        echo "<script>alert(\"hi\")</script>";
         header("location: ../login.php?error=invalid_login");
         exit();
     }
 
-    $pwdHashed = $userExists["password"];
+    $pwdHashed = $userExists["Password"];
     $checkPwd = password_verify($pwd, $pwdHashed);
 
     if($checkPwd === false){
@@ -54,17 +56,10 @@ function loginUser($conn, $uname, $pwd){
     }
     if($checkPwd === true){
         session_start();
-        $_SESSION['username'] = $userExists["username"];
-        $_SESSION['firstname'] = $userExists["firstname"];
-        $_SESSION['lastname'] = $userExists["lastname"];
+        $_SESSION['username'] = $userExists["Username"];
+        $_SESSION['firstname'] = $userExists["FirstName"];
+        $_SESSION['lastname'] = $userExists["LastName"];
         header('location: ../index.php');
         exit();
     }
 }
-
-if(emptyInputLogin($uname, $pwd) !== false){
-    header("location: ../login.php?error=emptyinput");
-    exit();
-}
-
-loginUser($conn, $uname, $pwd);
